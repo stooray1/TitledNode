1fdd5912e3d5182cf325c1c32cfa5f3782d13f2e
Tariq <mkha153@aucklanduni.ac.nz>
2017-02-23 09:59:36 +1300

    Popup will show on right cick. However on left click user can add joints

--- src/main/java/auckland/cs/TitledConnectionSkin.java
+++ src/main/java/auckland/cs/TitledConnectionSkin.java
@@ -7,13 +7,17 @@ package auckland.cs;
 
 import java.util.List;
 import java.util.Map;
-import de.tesis.dynaware.grapheditor.core.skins.defaults.connection.SimpleConnectionSkin;
+
+import de.tesis.dynaware.grapheditor.core.skins.defaults.DefaultConnectionSkin;
 import de.tesis.dynaware.grapheditor.model.GConnection;
 import javafx.geometry.Point2D;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import auckland.cs.Popup;
 
-public class TitledConnectionSkin extends SimpleConnectionSkin {
+public class TitledConnectionSkin extends DefaultConnectionSkin {
 	 private static final String STYLE_CLASS = "titled-connection";	 
 	 private static double normal_strok_width;
 	 private static double higlighted_strok_width;
@@ -27,7 +31,7 @@ public class TitledConnectionSkin extends SimpleConnectionSkin {
 
         super(connection);	              
         path.setMouseTransparent(true);
-        root.setOnMousePressed(this::handleMousePressed);
+        root.setOnMousePressed(this::handleMouseClicked);
         root.setOnMouseEntered(this::handleMouseEntered);
         root.setOnMouseExited(this::handleMouseExited); 
         
@@ -35,18 +39,21 @@ public class TitledConnectionSkin extends SimpleConnectionSkin {
         
         normal_strok_width = path.getStrokeWidth(); 
         higlighted_strok_width = normal_strok_width + 2.0;        
-    }    
-      
+    }          
     
     @Override
     public void draw(final List<Point2D> points, final Map<GConnection, List<Point2D>> allPoints) {
         super.draw(points, allPoints);	        
     }    
   
-    private void handleMousePressed(final MouseEvent event) {    	
-       Popup popup= new Popup(event.getScreenX(), event.getScreenY(), (Stage)root.getScene().getWindow(), getConnection().getId(), "Connection");     
-       popup.show();   
-    }	
+    private void handleMouseClicked(final MouseEvent event) {
+        if(event.getButton()== MouseButton.SECONDARY){
+     	   Popup popup= new Popup(event.getScreenX(), event.getScreenY(), (Stage)root.getScene().getWindow(), 
+     			                  getConnection().getId(), "Connection",  StageStyle.DECORATED);     
+     	   popup.show();
+     	  
+        }
+     }
     
     private void handleMouseEntered(final MouseEvent event){
     	this.path.setStrokeWidth(higlighted_strok_width);   	
